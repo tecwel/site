f75e8d96536a4687482e165ce61f53bf2ece1a62
Gaama Aala <gamingnotch4u@gmail.com>
2024-09-05 22:11:23 +0530

    i added

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const videoUrls = [
     'https://www.youtube.com/embed/1y4_tihWt2w',
     'https://www.youtube.com/embed/HyG5BaY7bNA',
     'https://www.youtube.com/embed/yl_pJD2gHUE',
+    'https://www.youtube.com/embed/nz3IXZ0saGQ',
     
 ];
 // // Adding event listeners for all video elements
